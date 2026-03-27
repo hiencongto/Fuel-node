a3b89f36a9bb6c18c51a58f36e6e884836fba5b8
HienHMT <hienahihi111@gmail.com>
2026-03-27 14:39:33 +0700

    add fuel API endpoint to retrieve fuel prices

---
+++ api/fuel.js
@@ -0,0 +1,20 @@
+const fuelService = require("../services/fuelService");
+
+module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
+  try {
+    const data = await fuelService.getFuelPrices();
+    res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
